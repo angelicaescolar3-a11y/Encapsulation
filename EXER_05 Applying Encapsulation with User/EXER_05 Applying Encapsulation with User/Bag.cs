b8a5ea5a9fcd8e1dde049f957080999501266193
ACT-STUDENT <ACT-STUDENT@LAB1-PC35>
2025-09-16 09:43:59 +0800

    EXER_05  Applying Encapsulation with User

---
+++ EXER_05 Applying Encapsulation with User/EXER_05 Applying Encapsulation with User/Bag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_05_Applying_Encapsulation_with_User
+{
+    internal class Bag
+    {
+        public string brand;
+        public int price;
+
+        //Set properties for the fields
+        public string Brand
+        {
+            get {  return brand; }
+            set { brand = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        //Method to display bag detials
+        public void DisplayBagDetails()
+        {
+            Console.WriteLine("The Brand " + brand);
+            Console.WriteLine("The Price " + price);
+        }
+    }
+}
---
+++ EXER_05 Applying Encapsulation with User/EXER_05 Applying Encapsulation with User/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_05_Applying_Encapsulation_with_User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the bag from the fields
+            Bag bag1 = new Bag();
+
+            //Setting properties using setter method
+            Console.Write("Enter the brand of the bag: ");
+            bag1.brand = Console.ReadLine();
+
+            Console.Write("Enter the price of the bag: ");
+            bag1.price = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //Call the method of the bag
+            bag1.DisplayBagDetails();
+
+            Console.ReadKey();
+        }
+    }
+}

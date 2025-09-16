a3e805f776104e4708c39f8f0649997a30744a83
ACT-STUDENT <ACT-STUDENT@LAB1-PC35>
2025-09-16 09:33:14 +0800

    EXER_04 Basics on Encapsulation

---
+++ EXER_04 Basics on Encapsulation/EXER_04 Basics on Encapsulation/Bag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_04_Basics_on_Encapsulation
+{
+    internal class Bag
+    {
+        private string brand;
+        public int price;
+
+        //Set properties for the fields
+        public string Brand
+        {
+            get {  return brand; }
+            set { brand = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        //Method to display bag Details
+        public void DisplayBagDetails()
+        {
+            Console.WriteLine("Brand: " + brand);
+            Console.WriteLine("Price: " + price);
+        }
+    }
+}
---
+++ EXER_04 Basics on Encapsulation/EXER_04 Basics on Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXER_04_Basics_on_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the Bag 
+            Bag bag1 = new Bag();
+
+            //Set properties using setter method
+            bag1.Brand = "Belinda";
+            bag1.price = 300;
+
+            //Call the method of the bag
+            bag1.DisplayBagDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
